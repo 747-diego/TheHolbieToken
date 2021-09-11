efe97473dcb102176293b07783ae9625b0a8ab4a
Nomad Engineers <nomadengineers@Diegos-MacBook-Pro.local>
2021-09-10 23:57:23 -0400

    [PREPPING ERC-20 FOR EXCHANGES]

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -21,8 +21,20 @@ contract DappToken {
         uint256 _value
     );
 
+    // TRANSFER EVENT
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+
     // MAPPING OF THE BALANCE OF OWNER AND RETURN UNSIGNED NUMBER
     mapping(address => uint256) public balanceOf;
+    // MAPPING OF ALLOWANCE
+    mapping(address => mapping(address => uint256)) public allowance;
+
+
 
     // DAPP TOKEN CONSTRUCTOR 
     constructor (uint256 _initialSupply) public {
@@ -42,6 +54,35 @@ contract DappToken {
         emit Transfer(msg.sender, _to, _value);
         // RETURN A BOOL
         return true;
+    }
+
+    // DELEGATED TRANSFERS
+
+    // APPROVE EVENT
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        // ALLOWANCE
+        allowance[msg.sender][_spender] = _value;
+        // APPROVE EVENT
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    // TRANSFER FROM
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        // REQUIRE _from HAS ENOUGH BALAMCE
+        // REQUIRE ALLOWANCE IS BIG ENOUGH 
+        // CHANGE THE BALANCE
+        balanceOf[_from] -= _value; // deducting balance from old account
+        balanceOf[_to] += _value; // adding balance to new account
+        // UPDATE THE ALLOWANCE
+        allowance[_from][msg.sender] -= _value;
+        // TRANSFER EVENT
+        emit Transfer(_from, _to, _value);
+        // RETURN A BOOLEAN
+        return true;
+        
     }
 
 
