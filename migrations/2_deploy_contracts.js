3b5b2f4d3dc664c38a2b5e8a43c84aee3a49ae52
Nomad Engineers <nomadengineers@Diegos-MacBook-Pro.local>
2021-09-13 20:38:56 -0400

    [CROWD SALE WITH SMART CONTRACT]

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var DappToken = artifacts.require("./DappToken.sol");
+var DappTokenSale = artifacts.require("./DappTokenSale.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(DappToken, 1000000);
+  deployer.deploy(DappToken, 1000000).then(function() {
+    // TOKEN PRICE IS 0.001 ETHER
+    var tokenPrice = 1000000000000000;
+    return deployer.deploy(DappTokenSale, DappToken.address, tokenPrice);
+  })
 };
